3adb4ea87871817dc7f3b3c540606e96d56e7892
Shazia_Idrees <vohragee@gmail.com>
2023-11-17 18:22:13 -0500

    Initial Commit

---
+++ JacketECommerceStore/JacketECommerceStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  JacketECommerceStoreApp.swift
+//  JacketECommerceStore
+//
+//  Created by Shazia Vohra on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct JacketECommerceStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
